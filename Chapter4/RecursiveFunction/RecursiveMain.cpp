55b00c15d32eea8ffce644926f6bdb81e718967c
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-07-29 01:20:13 +0900

    재귀함수 업데이트

---
+++ Chapter4/RecursiveFunction/RecursiveMain.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+void RecursiveLoop(int n);
+int RecursiveSum(int array[5], int start, int end);
+
+
+int main()
+{
+	RecursiveLoop(10);
+
+
+}
+
+void RecursiveLoop(int n)
+{
+	// base case
+	if (n == 0)
+	{
+		return;
+	}
+	// 이렇게 하면 1,2,3,4..... 순으로 출력된다
+	RecursiveLoop(n - 1);
+	std::cout << n << std::endl;
+
+	// 반대로 하면 1,2,3,4..... 순으로 출력된다
+	/*std::cout << n << std::endl;
+	RecursiveLoop(n - 1);*/
+
+	// 함수 호출 스택으로 인하여 이러한 결과가 산출된다. 
+}
+
+int RecursiveSum(int array[5], int start, int end)
+{
+	if (start == end)
+	{
+		return;
+	}
+	return array[start] + RecursiveSum(array, start + 1, end);
+}
