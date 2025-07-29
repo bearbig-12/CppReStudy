e78248c0ebb1d25025ac56fdff13ed0914fceb89
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-07-29 13:42:50 +0900

    재귀 함수 업데이트 + Fibonacci

--- Chapter4/RecursiveFunction/RecursiveMain.cpp
+++ Chapter4/RecursiveFunction/RecursiveMain.cpp
@@ -2,12 +2,22 @@
 
 void RecursiveLoop(int n);
 int RecursiveSum(int array[5], int start, int end);
-
+int Factorial(int n);
+int Fibonacci(int n);
 
 int main()
 {
-	RecursiveLoop(10);
+	//int array[]{1,2,3,4,5};
 
+	//int result1, result2{};
+	//result1 = RecursiveSum(array,0,4);
+	//std::cout << result1 << std::endl;
+	//
+	//result2 = Factorial(5);
+	//std::cout << result2 << std::endl;
+
+	
+	std::cout << Fibonacci(10);
 
 }
 
@@ -33,7 +43,25 @@ int RecursiveSum(int array[5], int start, int end)
 {
 	if (start == end)
 	{
-		return;
+		return array[start];
 	}
 	return array[start] + RecursiveSum(array, start + 1, end);
+}
+
+int Factorial(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	return n * Factorial(n - 1);
+}
+
+int Fibonacci(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return 1;
+	}
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
