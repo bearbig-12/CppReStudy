dcecbe694b13cc79c9aff1ae788f1ab3087504f6
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-09-16 13:00:33 +0900

    삽입 정렬, 병합 정렬 업데이트
    둘다 한번 더 공부하기

--- ChapterAlgorithm/Sort/sort.cpp
+++ ChapterAlgorithm/Sort/sort.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 void PrintArray(int input[], int size)
 {
@@ -62,12 +62,87 @@ void BubbleSort(int input[], int size)
 	}
 }
 
+void  InsertionSort(int input[], int size)
+{
+	for (int i = 1; i < size; ++i)
+	{
+		int key = input[i];
+		int j = i - 1;
+		while (j >= 0 && input[j] > key)
+		{
+			input[j + 1] = input[j];
+			--j;
+		}
+		input[j + 1] = key;
+	}
+}
+
+void Merge(int input[], int start, int half , int end, int temp[])
+{
+	int i = start;
+	int j = half + 1;
+	int tempIndex = 0;
+
+	// 왼쪽과 오른쪽 블럭 병합하기
+	while (i <= half && j <= end)
+	{
+		if (input[i] < input[j])
+		{
+			temp[tempIndex++] = input[i++];
+		}
+		else
+		{
+			temp[tempIndex++] = input[j++];
+		}
+	}
+
+
+	// 남은 인덱스 병합하기
+	while (i <= half)
+	{
+		temp[tempIndex++] = input[i++];
+	}
+
+	while (j <= end)
+	{
+		temp[tempIndex++] = input[j++];
+	}
+
+	tempIndex = 0;
+	// 기존 배열에 복사하기
+	for (int i = start; i <= end; ++i)
+	{
+		input[i] = temp[tempIndex++];
+	}
+
+
+}
+
+void MergeSort(int input[], int start, int end, int temp[])
+{
+	if (start == end)
+	{
+		return;
+	}
+
+	int half = (start + end) / 2;
+
+	MergeSort(input, start, half, temp);
+	MergeSort(input, half + 1, end, temp);
+	Merge(input, start, half, end, temp);
+}
+
 int main()
 {
-	const int size = 5;
-	int array[size]{ 8,7,2,3,1 };
+	const int size = 6;
+	int array[size]{ 8,7,2,3,1, 10 };
+	int temp[100]{};
 
-	BubbleSort(array, size);
+
+	//InsertionSort(array, size);
+
+	MergeSort(array, 0, 5, temp);
 
 	PrintArray(array, size);
+
 }
