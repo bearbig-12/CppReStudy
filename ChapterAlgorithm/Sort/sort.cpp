c9ccb14e90f92953f7d8164cdb44215a3f6315e6
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-09-25 06:07:06 +0900

    merge Again & Quick Sort
    머지는 계속 다시 풀어 보자

--- ChapterAlgorithm/Sort/sort.cpp
+++ ChapterAlgorithm/Sort/sort.cpp
@@ -83,6 +83,11 @@ void Merge(int input[], int start, int half , int end, int temp[])
 	int j = half + 1;
 	int tempIndex = 0;
 
+	for (int i = 0; i <= end; ++i)
+	{
+		std::cout << input[i] << std::endl;
+	}
+
 	// 왼쪽과 오른쪽 블럭 병합하기
 	while (i <= half && j <= end)
 	{
@@ -132,16 +137,148 @@ void MergeSort(int input[], int start, int end, int temp[])
 	Merge(input, start, half, end, temp);
 }
 
+
+
+void Merge2(int array[], int  start, int half, int end, int temp[])
+{
+	int left = start;
+	int right = half+1;
+	int tempIndex = 0;
+
+	while (left <= start && right <= end)
+	{
+		if (array[left] < array[right])
+		{
+			temp[tempIndex++] = array[left++];
+		}
+		else
+		{
+			temp[tempIndex++] = array[right++];
+		}
+	}
+
+	// 남은 인덱스 병합하기
+	while (left <= half)
+	{
+		temp[tempIndex++] = array[left++];
+	}
+
+	while (right <= end)
+	{
+		temp[tempIndex++] = array[right++];
+	}
+
+	tempIndex = 0;
+	// 기존 배열에 복사하기
+	for (int i = start; i <= end; ++i)
+	{
+		array[i] = temp[tempIndex++];
+	}
+
+}
+
+
+void MergetSort2(int array[], int start,  int end, int temp[])
+{
+
+	if (start == end)
+	{
+		return;
+	}
+	int half = (start + end + 1) / 2;
+
+	MergetSort2(array, start, half, temp);
+	MergetSort2(array, half+1, end, temp);
+	Merge2(array, start, half, end, temp);
+}
+
+
+
+void QuickSort(int input[], int left, int right)
+{
+	int pl = left;
+	int pr = right;
+	int pivot = input[(left + right) / 2];
+
+	do
+	{
+		while (input[pl] < pivot)
+		{
+			pl++;
+		}
+		while (input[pr] > pivot)
+		{
+			pr--;
+		}
+		if (pl <= pr)
+		{
+			Swap(input[pr], input[pl]);
+			pl++;
+			pr--;
+		}
+	} while (pl <= pr);
+
+	if (left < pr)
+	{
+		QuickSort(input, left, pr);
+	}
+	if (pl < right)
+	{
+		QuickSort(input, pl, right);
+	}
+
+}
+
+void QuickSort2(int input[], int left, int right)
+{
+	int pl = left;
+	int pr = right;
+	int pivot = input[(left + right) / 2];
+
+	do
+	{
+		while (input[pl] < pivot)
+		{
+			pl++;
+		}
+		while (input[pr] > pivot)
+		{
+			pr--;
+		}
+		if (pl <= pr)
+		{
+			Swap(input[pl], input[pr]);
+			pl++;
+			pr--;
+		}
+
+	} while (pl <= pr);
+
+	if (left < pr)
+	{
+		QuickSort2(input, left, pr);
+	}
+	if (pl < right)
+	{
+		QuickSort2(input, pl, right);
+	}
+}
+
+
+
+
 int main()
 {
-	const int size = 6;
-	int array[size]{ 8,7,2,3,1, 10 };
+	const int size = 5;
+	int array[size]{ 8,7,2,3,1};
 	int temp[100]{};
 
 
 	//InsertionSort(array, size);
 
-	MergeSort(array, 0, 5, temp);
+	//MergeSort(array, 0, 4, temp);
+
+	QuickSort2(array, 0, size - 1);
 
 	PrintArray(array, size);
 
